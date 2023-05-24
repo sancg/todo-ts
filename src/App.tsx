ed9e01f8a3590eb76f2f7c0210917c9ec19cc63a
Santiago Cano <sanblackx1@gmail.com>
2023-05-23 23:23:17 -0400

    Enhancement: SearchTodos working

--- src/App.tsx
+++ src/App.tsx
@@ -18,23 +18,33 @@ const defaultTodos: Todo = [
 ];
 
 function App() {
-    const [tasks, setTasks] = useState<Todo>(defaultTodos)
-    const [search, setSearch] = useState('')
+    const [tasks, setTasks] = useState<Todo>(defaultTodos);
+    const [search, setSearch] = useState("");
 
-    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.currentTarget.value)
-        setSearch(e.currentTarget.value)
-    }
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.currentTarget.value);
+        setSearch(e.currentTarget.value);
+    };
 
-    const completedTask = tasks.filter(item => item.completed).length
-    const totalTask = tasks.length
+    /** It was created a copy of the current state of Tasks
+     * And this would be render on the prop
+     *
+     */
+    const searchTodos: Todo = tasks.filter((task) => {
+        const searchText = search.toLocaleLowerCase();
+        const taskToFind = task.text.toLocaleLowerCase();
+        return taskToFind.includes(searchText);
+    });
+
+    const completedTask = tasks.filter((item) => item.completed).length;
+    const totalTask = tasks.length;
     return (
         <div className='App'>
             <TodoCounter completed={completedTask} totalTasks={totalTask} />
 
-            <TodoSearch search={search} onChange={handleChange}/>
+            <TodoSearch search={search} onChange={handleChange} />
             <TodoList>
-                {tasks.map((task) => (
+                {searchTodos.map((task) => (
                     <TodoItem key={task.text} task={task.text} />
                 ))}
             </TodoList>
