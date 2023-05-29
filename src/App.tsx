5ec01976db12f4fcd2b21b00f7a35ecf221ba066
Santiago Cano <sanblackx1@gmail.com>
2023-05-29 00:24:08 -0400

    Refactor: Provider must be at top level of the useContext()

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,39 @@ import TodoItem from "./components/TodoItem";
 import TodoList from "./components/TodoList";
 import TodoSearch from "./components/TodoSearch";
 import { AddButton } from "./components/AddTodoButton";
-import { TodoContext, TodoProvider } from "./context/TodoContext";
+import { TodoContext } from "./context/TodoContext";
 import "./App.css";
+import { useContext } from "react";
 
 function App() {
+    const {
+        todos,
+        search,
+        addTodo,
+        filterTodos,
+        handleCompleted,
+        handleDelete,
+        handleSearch: handleChangeSearch,
+        is_add: is_add_available,
+    } = useContext(TodoContext);
     return (
-        <TodoProvider>
-            <TodoContext.Consumer>
-                {({
-                    todos,
-                    search,
-                    addTodo,
-                    filterTodos,
-                    handleCompleted,
-                    handleDelete,
-                    handleSearch: handleChangeSearch,
-                    is_add: is_add_available,
-                }) => (
-                    <div className='App'>
-                        <TodoCounter tasks={todos} />
-                        <TodoSearch
-                            search={search}
-                            onChange={handleChangeSearch}
-                            onKeyPress={addTodo}
-                        />
-                        <TodoList>
-                            {filterTodos.map((task, i) => (
-                                <TodoItem
-                                    key={i + "-" + task.text}
-                                    text={task.text}
-                                    completed={task.completed}
-                                    onCompleted={() => handleCompleted(task.text)}
-                                    onDelete={() => handleDelete(task.text)}
-                                />
-                            ))}
-                        </TodoList>
+        <div className='App'>
+            <TodoCounter tasks={todos} />
+            <TodoSearch search={search} onChange={handleChangeSearch} onKeyPress={addTodo} />
+            <TodoList>
+                {filterTodos.map((task, i) => (
+                    <TodoItem
+                        key={i + "-" + task.text}
+                        text={task.text}
+                        completed={task.completed}
+                        onCompleted={() => handleCompleted(task.text)}
+                        onDelete={() => handleDelete(task.text)}
+                    />
+                ))}
+            </TodoList>
 
-                        <AddButton isTask={is_add_available} onClick={addTodo} />
-                    </div>
-                )}
-            </TodoContext.Consumer>
-        </TodoProvider>
+            <AddButton isTask={is_add_available} onClick={addTodo} />
+        </div>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { TodoProvider } from "./context/TodoContext.tsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <TodoProvider>
+            <App />
+        </TodoProvider>
+    </React.StrictMode>
+);
